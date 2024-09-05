0b12edb9e7eb6133ba3f00bbd3d4925d63dbc294
André Freitas <freitasandre38@gmail.com>
2024-09-05 09:52:47 -0300

    feat: create new create account use case and account gateway

---
+++ internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/andrefsilveira1/microservices/internal/entity"
+
+type AccountGateway interface {
+	Save(account *entity.Account) error
+	Find(id string) (*entity.Account, error)
+}
---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,44 @@
+package createaccount
+
+import (
+	"github.com/andrefsilveira1/microservices/internal/entity"
+	"github.com/andrefsilveira1/microservices/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientId string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(a gateway.AccountGateway, c gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: a,
+		ClientGateway:  c,
+	}
+}
+
+func (u *CreateAccountUseCase) Execute(input CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := u.ClientGateway.Get(input.ClientId)
+	if err != nil {
+		return nil, err
+	}
+
+	account := entity.NewAccount(client)
+	err = u.AccountGateway.Save(account)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
