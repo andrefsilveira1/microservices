75b3a51289e904b01c8867e9ae2b02729b76001a
André Freitas <freitasandre38@gmail.com>
2024-09-10 09:40:41 -0300

    fix: solve problems with client table creation

--- internal/database/client_db_test.go
+++ internal/database/client_db_test.go
@@ -18,7 +18,7 @@ func (s *ClientDBTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	s.Nil(err)
 	s.db = db
-	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), create_at date)")
+	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
 	s.clientDB = NewClientDb(db)
 }
 
